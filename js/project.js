7f0e63faf5a5026c259c4d91c211475d7719e0c5
Matt <matthew.salt@gmail.com>
2017-05-15 19:45:30 +0100

    Remove use of jquery for scroll highlighting. Fix bug in active link - need to use window.pageYOffset added to boundingClientRect().top + offsetHeight to get end pos. Use margin/padding hack to fix project jumpto anchor.

--- js/project.js
+++ js/project.js
@@ -1,33 +1,52 @@
-$.fn.inView = function(){
-    var win = $(window);
-    obj = $(this);
-    var scrollPosition = win.scrollTop();
-    var visibleArea = win.scrollTop() + win.height();
-    var objEndPos = (obj.offset().top + obj.outerHeight());
-    return(visibleArea >= objEndPos && (scrollPosition + 40)<= objEndPos ? true : false)
-};
+var getHeight = function(){
+    //window.innerHeight IE9+. Height (in pixels) of the browser window viewport including, if rendered, the horizontal scrollbar.
+    return window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
+}
+
+var getScrollTop = function(){
+    //window.pageYOffset - number of pixels scrolled from top of page. IE 9+
+    return (window.pageYOffset !== undefined) ? window.pageYOffset 
+                                              : (document.documentElement || document.body.parentNode || document.body).scrollTop
+}
+
+
+var inView = function(el){
+    var windowTop = getScrollTop();
+    var windowBottom = windowTop + getHeight()
+    var elTop = el.getBoundingClientRect().top + window.pageYOffset
+    var elBot = el.getBoundingClientRect().bottom + window.pageYOffset
+    if(windowTop + 100  > elBot){
+        return false
+    }
+
+    if(windowTop < elBot){
+        return true
+    }
+
+}
 
 var updateLinks = function(){
-    if($("#about").inView()) {
-        $("#aboutlink").addClass("active")   
-        $("#projectslink").removeClass("active")  
-        $("#contactlink").removeClass("active")  
-    } else if($("#projects").inView()) {
-        $("#aboutlink").removeClass("active") 
-        $("#projectslink").addClass("active")  
-        $("#contactlink").removeClass("active")  
-    }else if($("#contact").inView()) {
-        $("#aboutlink").removeClass("active")   
-        $("#projectslink").removeClass("active")
-        $("#contactlink").addClass("active")  
+    var aboutLink = document.getElementById("aboutlink")
+    var projectsLink = document.getElementById("projectslink")
+    var contactlink = document.getElementById("contactlink")
+    if(inView(document.getElementById("about"))) {
+        aboutLink.classList.add("active")   
+        projectslink.classList.remove("active")  
+        contactlink.classList.remove("active")  
+    } else if(inView(document.getElementById("projects"))) {
+        aboutLink.classList.remove("active") 
+        projectslink.classList.add("active")  
+        contactlink.classList.remove("active")  
+    }else if(inView(document.getElementById("contact"))) {
+        aboutLink.classList.remove("active")   
+        projectslink.classList.remove("active")
+        contactlink.classList.add("active")  
     }
 }
 
-$(window).scroll(function(){
-    updateLinks()
-});
+document.addEventListener('scroll', updateLinks)
 
-$(document).ready(function(){
-    console.log('active link')
-    $("#aboutlink").addClass("active") 
-})
+
+
+
+
